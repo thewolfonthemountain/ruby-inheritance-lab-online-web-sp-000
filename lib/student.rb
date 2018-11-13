806f92a1316a63215ced699ce0e373aec4cefc9a
Sean Langton <slangton1@gmail.com>
2018-11-13 02:15:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+attr_accessor :knowledge
+
+  def initialize
+    @knowledge =[]
+  end
+
+  def learn(lesson)
+    @knowledge << lesson
+  end
+
+#  def knowledge
 
 end
